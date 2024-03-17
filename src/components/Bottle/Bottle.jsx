6b8db96e9ccaf9ab5c9bf8421bbee933be442528
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-03-17 20:51:30 +0600

    explore handle add to cart

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,12 +1,13 @@
 import './Bottle.css';
-const Bottle = ({bottle}) => {
-    const {name, img, price, seller} = bottle;
+const Bottle = ({ bottle, handleAddToCart }) => {
+    const { name, img, price, seller } = bottle;
     return (
         <div className="bottle">
             <h3>Bottle: {name}</h3>
             <img src={img} alt="" />
             <p>Price: ${price}</p>
             <p>Seller: {seller}</p>
+            <button onClick={() => handleAddToCart(bottle)}>Purchase</button>
         </div>
     );
 };
